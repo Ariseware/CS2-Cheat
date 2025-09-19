b44b2b18c1dc477af671875bcfe9706690eef429
oqaisa <lowek795@gmail.com>
2025-09-19 19:23:24 +0200

    Added project structure

---
+++ target/debug/deps/cs2-2a29feca4feb05f3.d
@@ -0,0 +1,5 @@
+C:\Users\Gans\Desktop\AriseWare\Ariseware\Cs2\target\debug\deps\libcs2-2a29feca4feb05f3.rmeta: src\main.rs
+
+C:\Users\Gans\Desktop\AriseWare\Ariseware\Cs2\target\debug\deps\cs2-2a29feca4feb05f3.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/cs2-b091556c38704ed5.d
@@ -0,0 +1,5 @@
+C:\Users\Gans\Desktop\AriseWare\Ariseware\Cs2\target\debug\deps\libcs2-b091556c38704ed5.rmeta: src\main.rs
+
+C:\Users\Gans\Desktop\AriseWare\Ariseware\Cs2\target\debug\deps\cs2-b091556c38704ed5.d: src\main.rs
+
+src\main.rs:
